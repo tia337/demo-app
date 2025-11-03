df84cbcf7b9f01f9d24bbac8f3c1282424f42e17
Illia Telefus <ilya.it337@gmail.com>
2025-11-03 15:47:34 +0300

    chore: updated structure

--- libs/pages/util/src/lib/app-home.ts
+++ libs/pages/util/src/lib/app-home.ts
@@ -1,6 +1,7 @@
 import { inject, InjectionToken } from '@angular/core';
 
-import { NAV_ITEMS } from './nav-items/nav-items';
+import { NAV_ITEMS } from './nav-items';
+
 
 export const APP_HOME = new InjectionToken<string>('APP_HOME', {
   providedIn: 'root',
